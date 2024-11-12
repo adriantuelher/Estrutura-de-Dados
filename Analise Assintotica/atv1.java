72fab9ed79d32b6e796d603e88ac7f9358f3d5ee
Adrian Tuelher <153826191+adriantuelher@users.noreply.github.com>
2024-11-11 21:49:26 -0300

    Add files via upload

--- Analise Assintotica/atv1.java
+++ Analise Assintotica/atv1.java
@@ -1,58 +1,49 @@
-package aula2510;
-
-import java.util.Random;
 import java.util.Scanner;
 
 public class atv1 {
+    public static boolean estaAqui(int[] n, int a) {
+        int inicio = 0, fim = n.length - 1;
 
-    //O algoritimo tem complexidade linear ao tamanho do vetor
-    public static int[] maiorMenor(int n[]) {
-        int maior = n[0], menor = n[0];
+        
+        while (inicio <= fim) {
+            int metade = (inicio + fim) / 2;  
 
-        for (int i = 0; i < n.length; i++) {
-            if (maior < n[i]) {
-                maior = n[i];
+            if (n[metade] == a) {  
+                return true;
 
-            }else if (menor > n[i]) {
-                menor = n[i];
+            } else if (n[metade] < a) {  
+                inicio = metade + 1;  
+
+            } else {  
+                fim = metade - 1;  
 
             }
         }
 
-        return new int[] { maior, menor };
+        return false;  
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
 
         try {
-            int t;
-            Random random = new Random();
+            int a;
+            int[] n = { 1, 2, 3, 4, 5, 6, 7 };  
 
-            System.out.println("Informe o tamanho do vetor: ");
-            t = scanner.nextInt();
+            System.out.println("Escolha um número: ");
+            a = s.nextInt();
 
-            int n[] = new int[t];
-
-            for (int i = 0; i < t; i++) {
-                n[i] = random.nextInt(1000);
-
-                /*if (n[i] < 0) {
-                    n[i] = n[i] * -1;
-                }*/
-
-                System.out.println(n[i]);
+            
+            if (estaAqui(n, a)) {
+                System.out.println("O número que você escolheu está no vetor.");
+            } else {
+                System.out.println("O número que você escolheu não está no vetor.");
             }
-
-            int[] r = maiorMenor(n);
-            System.out.println("Maior número do vetor: " + r[0]);
-            System.out.println("Menor número do vetor: " + r[1]);
-
         } catch (Exception e) {
             System.out.println("Erro: " + e);
             main(args);
         }
 
-        scanner.close();
+        s.close();
     }
 }
