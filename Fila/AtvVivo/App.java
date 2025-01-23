228342092302893d58adcc5064afd6b15fed2a81
Adrian Tuelher <153826191+adriantuelher@users.noreply.github.com>
2025-01-23 09:23:21 -0300

    Add files via upload

---
+++ Fila/AtvVivo/App.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Fila fila = new Fila();
+        Carta c;
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            int qtdCartas = 0;
+            int numCartas = 1;
+            boolean t = true;
+
+            while (t) {
+                System.out.println("Quantas cartas: ");
+                qtdCartas = sc.nextInt();
+                if (qtdCartas >= 2 && qtdCartas <= 50) {
+                    while (qtdCartas > 0) {
+                        fila.enfileirar(new Carta(numCartas));
+                        numCartas++;
+                        qtdCartas--;
+                    }
+
+                    System.out.print("Discarded cards: ");
+                    while (fila.tamanho > 1) {
+                        c = fila.desenfileirar();
+                        if (fila.tamanho == 1) {
+                            System.out.print(c.numero);
+                            break;
+                        }
+                        System.out.print(c.numero + ", ");
+                        c = fila.desenfileirar();
+                        fila.enfileirar(c);
+
+                    }
+
+                    c = fila.desenfileirar();
+                    System.out.println("\nRemaining card: " + c.numero);
+
+                    t = false;
+                } else {
+                    System.out.println("Informe uma quantidade maior que 2 e menor que 50!");
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        sc.close();
+    }
+}
